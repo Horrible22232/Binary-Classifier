22b3e75284ab341f7244976e7e3f68daf5c71a0d
Horrible22232 <sorrybutiwin@web.de>
2022-07-04 15:53:24 +0200

    ++

---
+++ src/neuralnet/train.py
@@ -0,0 +1,35 @@
+import torch
+from docopt import docopt
+from trainer import Trainer
+from yaml_parser import YamlParser
+
+def main():
+    # Command line arguments via docopt
+    _USAGE = """
+    Usage:
+        train.py [options]
+        train.py --help
+    
+    Options:
+        --config=<path>            Path to the config file [default: ./configs/default.yaml]
+        --cpu                      Force training on CPU [default: False]
+    """
+    options = docopt(_USAGE)
+    cpu = options["--cpu"]
+    config = YamlParser(options["--config"]).get_config()
+
+    if not cpu:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            torch.set_default_tensor_type("torch.cuda.FloatTensor")
+    else:
+        device = torch.device("cpu")
+        torch.set_default_tensor_type("torch.FloatTensor")
+
+    # Initialize the PPO trainer and commence training
+    trainer = Trainer(config, device=device)
+    trainer.run_training()
+    trainer.close()
+
+if __name__ == "__main__":
+    main()
--- src/neuralnet/trainer.py
+++ src/neuralnet/trainer.py
@@ -1,2 +1,17 @@
+import torch
+
 class Trainer:
-    pass
+    def __init__(self, config:dict, device:torch.device) -> None:
+        """Initializes all needed training components.
+        Args:
+            config {dict} -- Configuration and hyperparameters of the trainer and model.
+            device {torch.device, optional} -- Determines the training device. Defaults to cpu.
+        """
+        self.config = config
+        self.device = device
+    
+    def run_training(self) -> None:
+        pass
+    
+    def close(self) -> None:
+        pass
