9203981070713ff471cf29b3ffd1bfdb28f99064
Horrible22232 <sorrybutiwin@web.de>
2022-07-04 17:10:42 +0200

    some more comments

--- src/neuralnet/test/testenv.py
+++ src/neuralnet/test/testenv.py
@@ -23,9 +23,14 @@ class SinEnv:
         If the label is 1, then the sinus funciton will be sampled at x_val else the cosinus function will be sampled.
         """
         for _ in range(num_samples):
+            # Generate random x-values between 0 and 1
             x_vals = np.random.uniform(low = 0.0, high=1.0, size=self.n)
+            # Generate random 0 or 1 labels
             labels = np.random.randint(2, size=self.n).astype(np.int).tolist()
+            # Evaluate the function at the x-values
             func_vals = [self.func[label](x) for x, label in zip(x_vals, labels)]
+            # Create the data tuple
             data = [[x_val, func_val] for x_val, func_val in zip(x_vals, func_vals)]
+            # Return the data and labels
             yield data, labels
         
