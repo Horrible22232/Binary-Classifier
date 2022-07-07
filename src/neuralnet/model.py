47f0c37688840bc248f05e5329814912651407ee
Horrible22232 <sorrybutiwin@web.de>
2022-07-07 20:35:37 +0200

    finished Recurrentvecencoder class

--- src/neuralnet/model.py
+++ src/neuralnet/model.py
@@ -15,16 +15,32 @@ class RecurrentVecEncoder(nn.Module):
         self.config = config
         self.vec_encoder = nn.Linear(in_features, self.config['hidden_size'])
         self.activ_fn = nn.ReLU()
-        self.recurrence = nn.GRU(self.config['hidden_size'], self.config['hidden_size'], batch_first=True)
+        if self.config["encoder"] == "lstm":
+            self.recurrent_layer = nn.LSTM(self.config["hidden_size"], self.config["hidden_state_size"], batch_first=True)
+        else:
+            self.recurrent_layer = nn.GRU(self.config['hidden_size'], self.config['hidden_state_size'], batch_first=True)
         
-    def forward(self, data: torch.Tensor) -> torch.Tensor:
+    def forward(self, h: torch.Tensor) -> torch.Tensor:
         """Encodes the input vector.
         Arguments:
             data {torch.Tensor} -- input sequence 
         Returns:
             {torch.Tensor} -- encoded sequence
         """
-        return data
+        # Get the number of sequences
+        num_sequences, sequence_len = h.shape[0], h.shape[1]
+        # Flatten the input vector
+        h = h.view(num_sequences * sequence_len, -1)
+        # Encode the input vector
+        h = self.activ_fn(self.vec_encoder(h))
+        # Reshape the encoded vector to the original shape
+        h = h.view(num_sequences, sequence_len, -1)
+        # Initialize the recurrent cell states
+        memory = self.init_recurrent_cell_states(num_sequences)
+        # Forward recurrent layer
+        h, memory = self.recurrent_layer(h, memory)
+        
+        return h
     
     def init_recurrent_cell_states(self, num_sequences:int) -> tuple:
         """Initializes the recurrent cell states (hxs, cxs) as zeros.
@@ -34,11 +50,12 @@ class RecurrentVecEncoder(nn.Module):
             {tuple} -- Depending on the used recurrent layer type, just hidden states (gru) or both hidden states and
                      cell states are returned using initial values.
         """
-        hxs = torch.zeros((num_sequences), self.config["hidden_size"], dtype=torch.float32, device=self.device).unsqueeze(0)
+        hxs = torch.zeros((num_sequences), self.config["hidden_state_size"], dtype=torch.float32, device=self.device).unsqueeze(0)
         cxs = None
         if self.config["encoder"] == "lstm":
-            cxs = torch.zeros((num_sequences), self.config["hidden_size"], dtype=torch.float32, device=self.device).unsqueeze(0)
-        return hxs, cxs
+            cxs = torch.zeros((num_sequences), self.config["hidden_state_size"], dtype=torch.float32, device=self.device).unsqueeze(0)
+            return hxs, cxs
+        return hxs
 
 class Classifier(nn.Module):
     def __init__(self, in_features, config: dict, device:torch.device) -> None:
